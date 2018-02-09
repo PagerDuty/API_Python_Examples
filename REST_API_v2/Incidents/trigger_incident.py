0c55d0763819883394d85a0294e7dbca44919198
Ashley Brooks <ashley.brooks.a@gmail.com>
2018-02-09 12:07:51 -0800

    Fixing spaces

--- REST_API_v2/Incidents/trigger_incident.py
+++ REST_API_v2/Incidents/trigger_incident.py
@@ -34,31 +34,31 @@ SERVICE_ID = ''
 FROM = ''
 
 def trigger_incident():
-	"""Triggers an incident using the V2 REST API."""
-	
+    """Triggers an incident using the V2 REST API."""
+    
     url = 'https://api.pagerduty.com/incidents'
     headers = {
-    	'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
         'Accept': 'application/vnd.pagerduty+json;version=2',
         'Authorization': 'Token token={token}'.format(token=API_KEY),
         'From': FROM
     }
 
     payload = {
-		"incident": {
-		    "type": "incident",
-		    "title": "The server is on fire.",
-		    "service": {
-		    	"id": SERVICE_ID,
-		      	"type": "service_reference"
-		    },
-		    "incident_key": "baf7cf21b1da41b4b0221008339ff3571",
-		    "body": {
-		      	"type": "incident_body",
-		      	"details": "A disk is getting full on this machine. You should investigate what is causing the disk to fill, and ensure that there is an automated process in place for ensuring data is rotated (eg. logs should have logrotate around them). If data is expected to stay on this disk forever, you should start planning to scale up to a larger disk."
-		    }
-		  }
-		}
+        "incident": {
+            "type": "incident",
+            "title": "The server is on fire.",
+            "service": {
+                "id": SERVICE_ID,
+                "type": "service_reference"
+            },
+            "incident_key": "baf7cf21b1da41b4b0221008339ff3571",
+            "body": {
+                "type": "incident_body",
+                "details": "A disk is getting full on this machine. You should investigate what is causing the disk to fill, and ensure that there is an automated process in place for ensuring data is rotated (eg. logs should have logrotate around them). If data is expected to stay on this disk forever, you should start planning to scale up to a larger disk."
+            }
+          }
+        }
 
     r = requests.post(url, headers=headers, data=json.dumps(payload))
 
