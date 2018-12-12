b5dae43f25d9020f95266198cfc9c46f36d781d8
Demitri Morgan <demitri@pagerduty.com>
2018-12-12 09:03:02 -0800

    Merged Files / fixed typos
    Python v3/v2 samples are adequately represented by the v3 samples, because the
    only difference as yet is print statement syntax (and Python 3's is backwards
    compatible), and there's little chance we will run into significant differences
    between the Python files to warrant doubling the number of files that will need
    to be maintained.
    
    Additionally, this commit fixes some typos (i.e. functions named
    `trigger_incident` where `resolve_incident` would be more appropriate).

---
+++ EVENTS_API_v2/ack/ack_incident.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+ROUTING_KEY = "" # ENTER EVENTS V2 API INTEGRATION KEY HERE 
+INCIDENT_KEY = "" # ENTER INCIDENT KEY HERE 
+
+def ack_incident():
+    # Triggers a PagerDuty incident without a previously generated incident key
+    # Uses Events V2 API - documentation: https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
+
+    header = {
+        "Content-Type": "application/json"
+    }
+
+    payload = { # Payload is built with the least amount of fields required to trigger an incident
+        "routing_key": ROUTING_KEY, 
+        "event_action": "acknowledge",
+        "dedup_key": INCIDENT_KEY
+    }
+
+    response = requests.post('https://events.pagerduty.com/v2/enqueue', 
+                            data=json.dumps(payload),
+                            headers=header)
+	
+    if response.json()["status"] == "success":
+        print('Incident Acknowledged ')
+    else:
+        print(response.text) # print error message if not successful
+
+if __name__ == '__main__':
+    ack_incident()
